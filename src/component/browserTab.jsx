1b18efeb0772e0497a35154fa28e6e63cc813a0e
mrinalsunny <mrinalsunny@gmail.com>
2020-04-16 01:59:45 +0530

    commit-8

--- src/component/browserTab.jsx
+++ src/component/browserTab.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Formik } from "formik";
 import {
   Card,
   ListGroup,
@@ -44,8 +45,25 @@ const BrowserTab = () => {
       password: "",
       owner: "",
     }).then((data) => {
+      setEditingBrowser(data);
       setBrowsers((browsers) => [...browsers, data]);
     });
+  };
+
+  const saveEdit = (props) => {
+    console.log(
+      props
+      // (browsers[browsers.findIndex((browser) => browser === editingBrowser)] = {
+      //   id: editingBrowser.id,
+      //   host: "158.234.207.103",
+      //   username: "",
+      //   password: "",
+      //   owner: "",
+      //   isEditable: "",
+      //   modifiedDate: "2020-04-14T09:54:43.128+0000",
+      // })
+    );
+    setEditingBrowser(null);
   };
 
   const renderBrowserEditModal = () => {
@@ -60,17 +78,41 @@ const BrowserTab = () => {
             animation={true}
             onHide={() => setEditingBrowser(null)}
           >
-            <Modal.Header>{editingBrowser.host}</Modal.Header>
-            <Modal.Body></Modal.Body>
-            <Modal.Footer>
-              <Button
-                variant="secondary"
-                onClick={() => setEditingBrowser(null)}
-              >
-                Close
-              </Button>
-              <Button variant="primary">Save changes</Button>
-            </Modal.Footer>
+            <Modal.Header>{editingBrowser.id}</Modal.Header>
+
+            <Formik
+              initialValues={{ host: editingBrowser.host }}
+              onSubmit={saveEdit}
+            >
+              {(props) => (
+                <form onSubmit={props.handleSubmit}>
+                  <Modal.Body>
+                    <input
+                      type="text"
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.host}
+                      name="host"
+                    />
+                  </Modal.Body>
+                  {props.errors.name && (
+                    <div id="feedback">{props.errors.host}</div>
+                  )}
+                  <Modal.Footer>
+                    <Button
+                      variant="secondary"
+                      onClick={() => setEditingBrowser(null)}
+                    >
+                      Close
+                    </Button>
+                    <Button type="submit" variant="primary">
+                      Save changes
+                    </Button>
+                  </Modal.Footer>
+                  {/* <Button type="submit">Submit</Button> */}
+                </form>
+              )}
+            </Formik>
           </Modal>
           {editingBrowser.host}
         </div>
